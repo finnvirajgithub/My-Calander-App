454dc4c7fce56a112ce565738d445da3eb7a49d8
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2024-05-21 16:03:48 +0530

    Design the one time page

--- app/src/main/java/com/example/today/splashscreen.java
+++ app/src/main/java/com/example/today/splashscreen.java
@@ -16,7 +16,7 @@ public class splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splashscreen.this, MainActivity.class);
+                Intent intent = new Intent(splashscreen.this, onetimeActivity.class);
                 startActivity(intent);
                 finish();
             }
