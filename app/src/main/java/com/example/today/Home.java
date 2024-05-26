2950f5fd4b20a65c21d320d9eba5aa41f8a0f794
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2024-05-26 17:23:04 +0530

    Add a bottom navigation

--- app/src/main/java/com/example/today/Home.java
+++ app/src/main/java/com/example/today/Home.java
@@ -1,35 +1,56 @@
 package com.example.today;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import com.example.today.R;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
 
-    Button logout;
+    BottomNavigationView bottomNavigationView;
+    CalendarFragment calendarFragment = new CalendarFragment();
+    AddFragment addFragment = new AddFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    InfoFragment infoFragment = new InfoFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        logout = findViewById(R.id.homelogout);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,calendarFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("remember","false");
-                editor.apply();
-
-                Intent intent = new Intent(Home.this, loginscreen.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()== R.id.calendar){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,calendarFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.add) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,addFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.profile) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.info) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,infoFragment).commit();
+                    return true;
+                }
+                return false;
             }
         });
+
+
     }
 }
